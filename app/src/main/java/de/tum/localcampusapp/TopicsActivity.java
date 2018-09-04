91643ae7ac8e9029ea179d39442cc7fc868b2030
Raphael Hetzel <raphael.hetzel@gmail.com>
2018-09-04 16:57:19 +0200

    Change to trigger ci

--- app/src/main/java/de/tum/localcampusapp/TopicsActivity.java
+++ app/src/main/java/de/tum/localcampusapp/TopicsActivity.java
@@ -2,11 +2,14 @@ package de.tum.localcampusapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class TopicsActivity extends AppCompatActivity {
+    static final String TAG = TopicsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d( TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topics);
     }

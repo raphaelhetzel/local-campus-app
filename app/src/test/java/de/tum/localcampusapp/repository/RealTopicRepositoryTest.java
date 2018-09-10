a890af7de3e2473e05db366bf9ebccf20ba31abc
Raphael Hetzel <raphael.hetzel@gmail.com>
2018-09-10 09:38:53 +0200

    Fix broken unit test

--- app/src/test/java/de/tum/localcampusapp/repository/RealTopicRepositoryTest.java
+++ app/src/test/java/de/tum/localcampusapp/repository/RealTopicRepositoryTest.java
@@ -1,9 +1,12 @@
 package de.tum.localcampusapp.repository;
 
+import android.database.SQLException;
+import android.database.sqlite.SQLiteConstraintException;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import de.tum.localcampusapp.database.TopicDao;
 import de.tum.localcampusapp.entity.Topic;
@@ -11,20 +14,23 @@ import de.tum.localcampusapp.exception.DatabaseException;
 
 import static org.mockito.Mockito.*;
 
-@RunWith(JUnit4.class)
+@RunWith(MockitoJUnitRunner.class)
 public class RealTopicRepositoryTest {
     TopicDao mTopicDao;
+    SQLiteConstraintException sqLiteConstraintException;
 
     @Before
     public void initialize_mocks() {
         mTopicDao = mock(TopicDao.class);
+        sqLiteConstraintException = mock(SQLiteConstraintException.class);
     }
 
     @Test(expected = de.tum.localcampusapp.exception.DatabaseException.class)
     public void insertWithDuplicateId() throws DatabaseException {
         RealTopicRepository realTopicRepository = new RealTopicRepository(mTopicDao);
         Topic topic2 = new Topic(1, "/tum/garching");
-        doThrow(new android.database.sqlite.SQLiteConstraintException()).when(mTopicDao).insert(topic2);
+        when(sqLiteConstraintException.getMessage()).thenReturn("UNIQUE constraint failed: topics.id (code 1555 SQLITE_CONSTRAINT_PRIMARYKEY)");
+        doThrow(sqLiteConstraintException).when(mTopicDao).insert(topic2);
         realTopicRepository.insertTopic(topic2);
     }
 }

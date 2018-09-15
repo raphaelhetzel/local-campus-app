a80eb70d51dc6bce007ed8ea27205b4a7abee523
Raphael Hetzel <raphael.hetzel@gmail.com>
2018-09-15 11:59:39 +0200

    Try out using the post extensions in the current state of the gui, commenting works (this commit can safely be discarded/overwritten)

--- app/src/main/java/de/tum/localcampusapp/Activities/PostCommentViewModel.java
+++ app/src/main/java/de/tum/localcampusapp/Activities/PostCommentViewModel.java
@@ -1,14 +1,17 @@
 package de.tum.localcampusapp.Activities;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Transformations;
 import android.arch.lifecycle.ViewModel;
 import android.content.Context;
 
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import de.tum.localcampusapp.entity.Post;
+import de.tum.localcampusapp.entity.PostExtension;
 import de.tum.localcampusapp.exception.DatabaseException;
 import de.tum.localcampusapp.repository.InMemoryPostRepository;
 import de.tum.localcampusapp.postTypes.Comment;
@@ -34,16 +37,21 @@ public class PostCommentViewModel extends ViewModel {
         liveDataPost = postRepository.getPost(postId);
 
         //Fake Comments for Fake Post
-        commentHelper = new CommentHelper();
-        FakeDataGenerator.getInstance().createSeveralFakeComments(3, commentHelper, postId, 1);
-        liveDataComments = commentHelper.getCommentsforPost(postId);
+//        commentHelper = new CommentHelper();
+//        FakeDataGenerator.getInstance().createSeveralFakeComments(3, commentHelper, postId, 1);
+//        liveDataComments = commentHelper.getCommentsforPost(postId);
+        liveDataComments = Transformations.map(postRepository.getPostExtensionsForPost(postId), postExtensions -> {
+           return postExtensions.stream().map(postExtension -> {
+                return new Comment(postId,
+                        1,
+                        postExtension.getData(),
+                        postExtension.getCreatedAt());
+           }).collect(Collectors.toList());
+        });
     }
 
     public void addComment(String commentData) throws DatabaseException {
-            long id = FakeDataGenerator.getInstance().getPostId();
-            long commentId = FakeDataGenerator.getInstance().getCommentId();
-
-            commentHelper.insertComment(new Comment(postId, commentId, commentData, new Date()));
+            postRepository.addPostExtension(new PostExtension(postId, commentData));
     }
 
     public LiveData<Post> getLiveDataPost() {
